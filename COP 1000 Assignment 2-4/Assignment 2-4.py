cf7098ef338fa0ebeb6228dc303976b90a932c6d
Jagermind <jagerunz@gmail.com>
2024-09-13 13:53:39 -0400

    Added Tax/Deductioin Maths and Print Statements

--- COP 1000 Assignment 2-4/Assignment 2-4.py
+++ COP 1000 Assignment 2-4/Assignment 2-4.py
@@ -1,9 +1,18 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
-
+salary = float(input("Please Input Salary:"))
+numDependents = float(input("Please Input Number of Dependents:"))
+stateTaxRate =.065
+federalTaxRate =.28
+dependentDeductionRate = .025
 # calculate taxes here
-
+stateTax = salary * stateTaxRate
+federalTax = salary * federalTaxRate
+dependentDeduction =  salary * (dependentDeductionRate * numDependents)
+totalWitholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWitholding
 # output statements
+print ("State Tax:" + str(stateTax))
+print ("Federal Tax" + str(federalTax))
+print ("Dependents:" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
